079ce590a5aa57cdf8bda8d551c798a81a875155
GraceChelangat <166394793+GraceChelangat@users.noreply.github.com>
2024-11-18 16:07:08 +0300

    Update script.sql
    Signed-off-by: GraceChelangat <166394793+GraceChelangat@users.noreply.github.com>

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,1 +1,18 @@
 -- Write your SQL code here
+CREATE TRIGGER UpdateProgramStatus  
+AFTER INSERT ON Funding  
+FOR EACH ROW  
+BEGIN  
+    UPDATE Program  
+    SET status = 1  
+    WHERE program_id = NEW.program_id AND status = 0;  
+END;  
+
+CREATE TRIGGER UpdateFundingTotal  
+AFTER INSERT ON Funding  
+FOR EACH ROW  
+BEGIN  
+    UPDATE Program  
+    SET total_funding_received = total_funding_received + NEW.amount  
+    WHERE program_id = NEW.program_id;  
+END;  
