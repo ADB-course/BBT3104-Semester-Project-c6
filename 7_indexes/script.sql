3a7d0775a288109142f001a1da8d9f52af0de14d
Raggz291 <146196337+Raggz291@users.noreply.github.com>
2024-11-18 22:32:30 +0300

    Update script.sql
    Signed-off-by: Raggz291 <146196337+Raggz291@users.noreply.github.com>

--- 7_indexes/script.sql
+++ 7_indexes/script.sql
@@ -1,1 +1,8 @@
 -- Write your SQL code here
+-- Creating indexes for the Funding and Program tables
+
+-- Funding Table: Index on DonorID (foreign key)
+CREATE INDEX idx_donor_id ON Funding(DonorID);
+
+-- Program Table: Index on ProgramStatus
+CREATE INDEX idx_program_status ON Program(ProgramStatus);
